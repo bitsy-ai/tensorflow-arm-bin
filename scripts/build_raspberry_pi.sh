6ae16db690cb3b20708f63a4f461370f558b20fe
Leigh Johnson <hi@leighjohnson.me>
2020-11-07 19:29:31 -0800

    add build script

---
+++ scripts/build_raspberry_pi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set +x
+
+TENSORFLOW=$HOME/builds/tensorflow
+HEIGHT=20
+WIDTH=60
+CHOICE_HEIGHT=4
+BACKTITLE="Build Raspberry Pi .whl"
+TITLE="Choose processor architechture"
+MENU="Choose one of the following options:"
+TERMINAL=$(tty)
+OPTIONS=(1 "arm7l 32-bit"
+         2 "aarch64 64-bit")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >$TERMINAL)
+
+clear
+case $CHOICE in
+        1)
+            cd $TENSORFLOW && $TENSORFLOW/tensorflow/tools/ci_build/ci_build.sh PI-PYTHON37 \
+                tensorflow/tools/ci_build/pi/build_raspberry_pi.sh
+            ls -ahl output-artifacts/
+            ;;
+        2)
+            cd $TENSORFLOW && $TENSORFLOW/tensorflow/tools/ci_build/ci_build.sh PI-PYTHON37 \
+                tensorflow/tools/ci_build/pi/build_raspberry_pi.sh AARCH64
+            ls -ahl output-artifacts/
+            ;;
+esac
